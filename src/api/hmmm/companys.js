173daba74ee51523b4e0d1d5ad2bd5e4afe1abe8
lyx <15205156886@163.com>
2022-04-04 02:57:25 +0800

    张辉功能开发

--- src/api/hmmm/companys.js
+++ src/api/hmmm/companys.js
@@ -7,13 +7,20 @@
  * 企业管理
  */
 
+// import { createAPI } from '@/utils/request'
+// //
+// export const list = data => createAPI('/companys', 'get', data)
+// export const add = data => createAPI('/companys', 'post', data)
+// export const update = data => createAPI(`/companys/${data.userId}`, 'get', data)
+// export const remove = data => createAPI(`/companys/${data}`, 'DELETE')
+// export const detail = data => createAPI(`/companys/${data.userId}`, 'post', data)
+// export const disabled = data => createAPI(`/companys/${data.id}/${data.state}`, 'post', data)
+
 import { createAPI } from '@/utils/request'
 
-export const list = data => createAPI('/comapnys', 'post')
-export const add = data => createAPI('/comapnys', 'post', data)
-export const update = data =>
-  createAPI(`/comapnys/${data.userId}`, 'get', data)
-export const remove = data => createAPI(`/comapnys/${data.id}`, 'get')
-export const detail = data =>
-  createAPI(`/comapnys/${data.userId}`, 'post', data)
-export const disabled = data => createAPI('/comapnys/state', 'post', data)
+export const list = data => createAPI('/companys', 'get', data) // zh
+export const add = data => createAPI('/companys', 'post', data)// zh
+export const update = data => createAPI(`/companys/${data.userId}`, 'put', data)// zh
+export const remove = data => createAPI(`/companys/${data}`, 'DELETE')// zh
+export const detail = data => createAPI(`/companys/${data}`, 'get', data)// zh
+export const disabled = data => createAPI(`/companys/${data.id}/${data.state}`, 'post', data)// zh
